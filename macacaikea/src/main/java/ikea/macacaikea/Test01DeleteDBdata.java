4a0431eb03cef369092f14382c5eaa5092285ef1
Jasmine Qian <37689567+JasmineQian@users.noreply.github.com>
2018-05-08 10:13:27 +0800

    Update Test01DeleteDBdata.java

--- macacaikea/src/main/java/ikea/macacaikea/Test01DeleteDBdata.java
+++ macacaikea/src/main/java/ikea/macacaikea/Test01DeleteDBdata.java
@@ -9,9 +9,9 @@ import java.sql.Statement;
 public class Test01DeleteDBdata {
 
 	// 这里可以设置数据库名称
-	private final static String URL = "jdbc:sqlserver://NTGQASQL03:1433;DatabaseName=IKEA_MZN_QA";
-	private static final String USER = "IKEA_MZN_QA";
-	private static final String PASSWORD = "C935DD58";
+	private final static String URL = "jdbc:sqlserver:/hhh";
+	private static final String USER = "jjj";
+	private static final String PASSWORD = "xxxx";
 
 	private static Connection conn = null;
 	// 静态代码块（将加载驱动、连接数据库放入静态块中）
@@ -48,23 +48,7 @@ public class Test01DeleteDBdata {
 		}
 
 		String sql = "SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_mobile = '18962873440'\r\n" + "\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN WHERE CST_ID BETWEEN 298 AND 314\r\n" + "\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_ID = 247\r\n" + "\r\n" + "\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_MOBILE in ('17321135211','18717805676','18817356097''18001701210''15618465639')\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_MOBILE in ('17321135211','18001701210')\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_MOBILE ='17721658628' and CST_DELETED_FLAG= 1\r\n"
-				+ "--select * from #TEMPCARDKKK\r\n"
-				+ "--SELECT CST_ID INTO  #TEMPCARDKKK FROM BCUSTOMER_MZN where CST_NAME like '%测试%'\r\n" + "\r\n"
-				+ "--select * from BCUSTOMER_MZN where CST_DELETED_FLAG=1\r\n" + "\r\n"
-				+ "DELETE FROM BCHILD_MZN WHERE CLD_XCS_CST_ID IN (SELECT CST_ID  FROM  #TEMPCARDKKK )\r\n"
-				+ "DELETE FROM XCUSTOMER_MZN WHERE XCS_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK )\r\n"
-				+ "DELETE FROM BSMLACTIVESIGNIN where SAS_MMB_ID in  (SELECT MMB_ID FROM BMEMBERSHIP_MZN WHERE MMB_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK ))\r\n"
-				+ "DELETE FROM XMEMBERSHIP_MZN  WHERE XMB_MMB_ID IN (SELECT MMB_ID FROM BMEMBERSHIP_MZN WHERE MMB_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK ))\r\n"
-				+ "DELETE FROM HCOUPON_HISTORY WHERE HCP_MMB_ID IN (SELECT MMB_ID FROM BMEMBERSHIP_MZN WHERE MMB_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK ))\r\n"
-				+ "DELETE FROM RCOUPON_MEMBERSHIP WHERE CPM_MMB_ID IN (SELECT MMB_ID FROM BMEMBERSHIP_MZN WHERE MMB_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK ))\r\n"
-				+ "DELETE FROM BMEMBERSHIP_MZN WHERE MMB_CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK )\r\n"
-				+ "DELETE FROM BCUSTOMER_MZN WHERE CST_ID IN (SELECT CST_ID FROM #TEMPCARDKKK) ";
-		Statement stmtdelete = conn.createStatement();// 创建Statement对象
+			Statement stmtdelete = conn.createStatement();// 创建Statement对象
 		stmtdelete.executeUpdate(sql);// 执行sql语句
 
 		Logger.Output(LogType.LogTypeName.INFO, "指定会员已经删除 " );
