2de346b92202d231700f9804659e43de6f438d1e
Jasmine Qian <37689567+JasmineQian@users.noreply.github.com>
2018-05-08 10:14:21 +0800

    Update Propertyikea.java

--- macacaikea/src/main/java/ikea/macacaikea/Propertyikea.java
+++ macacaikea/src/main/java/ikea/macacaikea/Propertyikea.java
@@ -3,11 +3,11 @@ package ikea.macacaikea;
 public class Propertyikea {
 	
 	// 10.215.70.209
-		public static final String URL_IKEA = "https://yijiauat.acxiom.com.cn/ikeaMemberZoneV2/index.action?store=512&chanel=001";
-		public static final String smscode = "631029"; 
-		public static final String validcode = "631029"; 
-		public static final String username = "钱金燕"; 
-		public static final String mobilenum = "18962873440"; 
+		public static final String URL_IKEA = "https://hh";
+		public static final String smscode = "1111"; 
+		public static final String validcode = "1111"; 
+		public static final String username = "1111"; 
+		public static final String mobilenum = "1111"; 
 		public static final int waitfortime = 2;
 
 		// 注册新会员
